a6bae86dfa276e2fd72132af7af4d0f957bfa7ef
alejandrobr <alejandro.reyes@alumno.uttehuacan.edu.mx>
2019-02-26 16:55:52 +0100

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/fr/acanoen/touchalert/MainActivity.java
+++ app/src/main/java/fr/acanoen/touchalert/MainActivity.java
@@ -1,5 +1,6 @@
 package fr.acanoen.touchalert;
 
+import android.app.FragmentManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -8,13 +9,31 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener, BoardFragment.OnFragmentInteractionListener, AlertFragment.OnFragmentInteractionListener, NotificationFragment.OnFragmentInteractionListener {
 
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private TextView mTextMessage;
+    private MapFragment mapFragment;
+    private GoogleMap map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
         loadFragment(new BoardFragment());
 
         BottomNavigationView navigation = findViewById(R.id.navigation);
@@ -37,6 +56,13 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         Fragment fragment = null;
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         switch (item.getItemId()) {
             case R.id.navigation_home:
                 fragment = new BoardFragment();
@@ -52,7 +78,18 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+    map= googleMap;
+    }
+    @Override
     public void onFragmentInteraction(Uri uri) {
 
     }
 }
+
